961a70b4d61f827c57d6b415e7fe5b0db8226d9c
Ulrik Stridsman <uforsgre@cisco.com>
2023-12-14 15:57:22 +0100

    Add code to send a simple notification.

--- packages/skylight/netsim/skylight.py
+++ packages/skylight/netsim/skylight.py
@@ -8,31 +8,86 @@
 *********************************************************************
 """
 
+from datetime import datetime
 import logging
 import os
+import random
+import socket
 import sys
 import time
 
-
+import _ncs
 import ncs
 from ncs.dp import Action, Daemon
 from ncs.maapi import Maapi
 from ncs.log import Log
 
+from skylight_netsim_ns import ns
+
+xmltag = _ncs.XmlTag
+value = _ncs.Value
+tagvalue = _ncs.TagValue
 
 
-#logger class used by Daemon
-class MyLog(object):
-    def info(self, arg):
-        print("info: %s" % arg)
-    def error(self, arg):
-        print("error: %s" % arg)
+def get_date_time():
+    now = datetime.now()
+    confdNow = _ncs.DateTime(
+        year=now.year,
+        month=now.month,
+        day=now.day,
+        hour=now.hour,
+        min=now.minute,
+        sec=now.second,
+        micro=now.microsecond,
+        timezone=0,
+        timezone_minutes=0)
+    return confdNow
+
+
+notif_daemon = None
+
+
+class NotificationDaemon:
+    def __init__(self, name, stream):
+        self.ndaemon = ncs.dp.Daemon(name)
+        self.nsock = socket.socket()
+        _ncs.dp.connect(self.ndaemon.ctx(), self.nsock,
+                            _ncs.dp.WORKER_SOCKET, "127.0.0.1", ncs.PORT)
+        self.nctx = _ncs.dp.register_notification_stream(self.ndaemon.ctx(),
+                                      None, self.nsock, stream)
+    def start(self):
+        self.ndaemon.start()
+    def finish(self):
+        self.ndaemon.finish()
+    def join(self):
+        self.ndaemon.join()
+
+    def send(self):
+        values = [
+            tagvalue(xmltag(ns.hash,
+                            ns.skylight_netsim_skylight_event),
+                     value((ns.skylight_netsim_skylight_event, ns.hash),
+                           _ncs.C_XMLBEGIN)
+                     ),
+            tagvalue(xmltag(ns.hash,
+                            ns.skylight_netsim_random),
+                     value(random.randint(1, 1000000), _ncs.C_UINT32)),
+
+            tagvalue(xmltag(ns.hash,
+                            ns.skylight_netsim_skylight_event),
+                     value((ns.skylight_netsim_skylight_event, ns.hash),
+                           _ncs.C_XMLEND)
+                     )
+        ]
+        _ncs.dp.notification_send(self.nctx, get_date_time(), values)
 
 
 class SendNotificationAction(Action):
     @Action.action
     def cb_action(self, uinfo, name, kp, input, output):
-        self.log.info("Notification sent")
+        if notif_daemon is not None:
+            notif_daemon.send()
+            self.log.info("Notification sent")
 
 
 def load_schemas():
@@ -48,26 +103,38 @@ if __name__ == "__main__":
 
     load_schemas()
     log = Log(logger, add_timestamp=True)
+
+    daemons = []
+
     d = Daemon(name='skylight-netsim', log=log)
+    daemons.append(d)
 
     a = []
     a.append(SendNotificationAction(daemon=d,
                        actionpoint='skylight-send-notification', log=log))
 
+    notif_daemon = NotificationDaemon("skylight-notification-daemon",
+                                        "skylight-events")
+    daemons.append(notif_daemon)
+
     log.info('--- Daemon Skylight Netsim STARTED ---')
+
     try:
-        d.start()
+        for daemon in daemons:
+            daemon.start()
 
         while True:
             d.join(1)
             if not d.is_alive():
+                daemons.remove(d)
                 break
     except Exception as e:
         print("ERROR:", e)
 
 
-    #if not d.isAlive():
-    #    d.finish()
-    #    d.join()
+    for daemon in daemons:
+        daemon.finish()
+        daemon.join()
+
 
     log.info('--- Daemon myaction FINISHED ---')
