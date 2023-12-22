719be07f69572403febba9cdfb0f9a9ae214448e
Per Andersson <perander@cisco.com>
2023-12-22 19:53:33 +0100

    edge: Process for dp values
    * All values are handled in a process.
    
    * Toggle jitter.

--- packages/edge/netsim/edge.erl
+++ packages/edge/netsim/edge.erl
@@ -3,6 +3,8 @@
 -include_lib("econfd.hrl").
 -include("econfd_errors.hrl").
 
+-export([toggle_jitter/0]).
+
 -on_load(on_load/0).
 
 on_load() ->
@@ -23,7 +25,6 @@ start_log() ->
 
 
 init() ->
-    %% wait for confd to come up
     print("waiting for confd to start~n", []),
     timer:sleep(1000),
 
@@ -32,6 +33,7 @@ init() ->
     print("CONFD_IPC_PORT = ~p~n", [ConfdPort]),
     {ok, Daemon} = econfd:init_daemon(edge, ?CONFD_TRACE, user, none,
                                       {127,0,0,1}, ConfdPort),
+    register(edge_dp, self()),
 
     TransCbs = #confd_trans_cbs{init = fun init_trans/1},
     ok = econfd:register_trans_cb(Daemon, TransCbs),
@@ -40,37 +42,69 @@ init() ->
                               get_elem  = fun get_elem/2},
     ok = econfd:register_data_cb(Daemon, DataCbs),
 
+    %% FIXME
+    %% * add toggle_jitter rpc
+
     ok = econfd:register_done(Daemon),
 
-    loop().
+    loop([{high_jitter, false}]).
 
-
-loop() ->
-    receive
-    after infinity ->
-              ok
-    end.
-
-init_trans(_) -> ok.
 
 %% FIXME
 %% * separate total and per content metrics
 %% * mutliply per content metrics with length of list
 %% * set high and low jitter
-get_elem(_Tx, ['buffer-fill' | _]) ->
-    Base = 70,
-    Rand = rand:uniform(30),
-    {ok, {?C_UINT8, Base + Rand}};
-get_elem(_Tx, [jitter | _]) ->
-    FractionDigits = 3,
-    Base = 0,
-    Rand = abs(rand:normal()),
-    Jitter = abs(trunc((Base + Rand) * 1000)),
-    {ok, {?C_DECIMAL64, {Jitter, FractionDigits}}};
-get_elem(_Tx, _Path) ->
-    Base = 10 * 1024 * 1024,
-    Rand = rand:uniform(1024 * 1024),
-    {ok, {?C_UINT64, Base + Rand}}.
+loop([{high_jitter, HighJitter}] = State) ->
+    receive
+        {From, toggle_jitter} ->
+            From ! {edge_dp, {ok, not HighJitter}},
+            loop([{high_jitter, not HighJitter}]);
+
+        {From, {get_elem, ['buffer-fill' | _]}} ->
+            Base = 70,
+            Rand = rand:uniform(30),
+            From ! {edge_dp, {ok, {?C_UINT8, Base + Rand}}},
+            loop(State);
+
+        {From, {get_elem, [jitter | _]}} ->
+            FractionDigits = 3,
+            Base =
+                case HighJitter of
+                    true  -> 10;
+                    false -> 0
+                end,
+            Rand = abs(rand:normal()),
+            Jitter = abs(trunc((Base + Rand) * 1000)),
+            From ! {edge_dp, {ok, {?C_DECIMAL64, {Jitter, FractionDigits}}}},
+            loop(State);
+
+        {From, {get_elem, _Path}} ->
+            Base = 10 * 1024 * 1024,
+            Rand = rand:uniform(1024 * 1024),
+            From ! {edge_dp, {ok, {?C_UINT64, Base + Rand}}},
+            loop(State)
+    end.
+
+
+init_trans(_) -> ok.
+
+get_elem(_Tx, Path) ->
+    edge_dp ! {self(), {get_elem, Path}},
+    receive
+        {edge_dp, Res} ->
+            Res
+    after 1000 ->
+              not_found
+    end.
+
+toggle_jitter() ->
+    edge_dp ! {self(), toggle_jitter},
+    receive
+        {edge_dp, Res} ->
+            Res
+    after 1000 ->
+              error
+    end.
 
 
 print(Fmt, Args) ->
