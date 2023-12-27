74dec8cc35552a25de4ee2d1104408a56d523be7
Per Andersson <perander@cisco.com>
2023-12-27 12:09:38 +0100

    origin: Dynamic metrics
    All metrics are now based on number of instances in /origin/content
    list.

--- packages/origin/netsim/origin.erl
+++ packages/origin/netsim/origin.erl
@@ -3,6 +3,11 @@
 -include_lib("econfd.hrl").
 -include("econfd_errors.hrl").
 
+
+-define(NS, 'http://tail-f.com/ns/cleu24/tecops-2665/origin').
+
+-record(state, {maapi :: econfd:socket()}).
+
 -on_load(on_load/0).
 
 on_load() ->
@@ -23,7 +28,6 @@ start_log() ->
 
 
 init() ->
-    %% wait for confd to come up
     print("waiting for confd to start~n", []),
     timer:sleep(1000),
 
@@ -32,6 +36,10 @@ init() ->
     print("CONFD_IPC_PORT = ~p~n", [ConfdPort]),
     {ok, Daemon} = econfd:init_daemon(origin, ?CONFD_TRACE, user, none,
                                       {127,0,0,1}, ConfdPort),
+
+    register(origin_dp, self()),
+
+    {ok, MaapiSock} = econfd_maapi:connect({127,0,0,1}, ConfdPort),
 
     TransCbs = #confd_trans_cbs{init = fun init_trans/1},
     ok = econfd:register_trans_cb(Daemon, TransCbs),
@@ -42,28 +50,66 @@ init() ->
 
     ok = econfd:register_done(Daemon),
 
-    loop().
+    loop(#state{maapi = MaapiSock}).
 
 
-loop() ->
+%% * separate total and per content metrics
+%% * mutliply per content metrics with length of list
+loop(#state{maapi = MaapiSock} = State) ->
     receive
-    after infinity ->
-              ok
+        %% per content metrics
+        {From, {get_elem, ['buffer-fill', {_}, content | _]}} ->
+            From ! {origin_dp, {ok, {?C_UINT8, buffer_fill(1)}}},
+            loop(State);
+
+        {From, {get_elem, [_Leaf, {_}, content | _]}} ->
+            From ! {origin_dp, {ok, {?C_UINT64, traffic(1)}}},
+            loop(State);
+
+        %% totals metrics
+        {From, {get_elem, ['buffer-fill' | _]}} ->
+            N = num_instances(MaapiSock),
+            From ! {origin_dp, {ok, {?C_UINT8, buffer_fill(N)}}},
+            loop(State);
+
+        {From, {get_elem, _Path}} ->
+            N = num_instances(MaapiSock),
+            From ! {origin_dp, {ok, {?C_UINT64, traffic(N)}}},
+            loop(State)
     end.
 
 init_trans(_) -> ok.
 
-%% FIXME
-%% * separate totals and per content metrics.
-%% * multiply content metrics by length of content list
-get_elem(_Tx, ['buffer-fill' | _]) ->
-    Base = 70,
-    Rand = rand:uniform(30),
-    {ok, {?C_UINT8, Base + Rand}};
-get_elem(_Tx, _Path) ->
+get_elem(_Tx, Path) ->
+    origin_dp ! {self(), {get_elem, Path}},
+    receive
+        {origin_dp, Res} ->
+            Res
+    after 1000 ->
+              not_found
+    end.
+
+
+%% uint8
+buffer_fill(0) -> 0;
+buffer_fill(_) -> 70 + rand:uniform(30).
+
+%% uint64
+traffic(N) ->
     Base = 10 * 1024 * 1024,
     Rand = rand:uniform(1024 * 1024),
-    {ok, {?C_UINT64, Base + Rand}}.
+    N * (Base + Rand).
+
+num_instances(MaapiSock) ->
+    ok = econfd_maapi:start_user_session(MaapiSock, <<"admin">>, <<"maapi">>,
+                                         [<<"admin">>], {127,0,0,1},
+                                         ?CONFD_PROTO_TCP),
+    {ok, Th} = econfd_maapi:start_trans(MaapiSock, ?CONFD_RUNNING, ?CONFD_READ),
+    {ok, Num} =
+        econfd_maapi:num_instances(MaapiSock, Th, [content, [?NS|origin]]),
+    ok = econfd_maapi:finish_trans(MaapiSock, Th),
+    ok = econfd_maapi:end_user_session(MaapiSock),
+    Num.
 
 
 print(Fmt, Args) ->
