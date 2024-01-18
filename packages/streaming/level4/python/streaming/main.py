0ce347ff09b876d759fcc8377e6a9a1bb5e4edff
Ulrik Stridsman <uforsgre@cisco.com>
2024-01-18 20:03:53 +0100

    Add agentIds to data center. Fix notification callback. Add python nano-callback. Set skylight-agent-id to be just a string. Dynamically set session-id.

--- packages/streaming/level4/python/streaming/main.py
+++ packages/streaming/level4/python/streaming/main.py
@@ -12,11 +12,23 @@ from ncs.maapi import Maapi
 from ncs.dp import Action
 
 import traceback
+import uuid
+
+
+class ConnectedToSkylight(NanoService):
+    @NanoService.create
+    def cb_nano_create(self, tctx, root, service, plan, component, state, proplist, compproplist):
+        self.log.info(f'cb_nano_create: ConnectedToSkylight')
+        vars = ncs.template.Variables()
+        vars.add('SESSION_ID', str(uuid.uuid1()))
+        vars.add('DC', "dc0")
+        template = ncs.template.Template(service)
+        template.apply('edge-servicepoint-edge-connected-to-skylight', vars)
 
 
 class SkylightNotificationAction(Action):
     @Action.action
-    def do_action(self, t, input):
+    def cb_action(self, t, input):
         try:
             root = ncs.maagic.get_root(t)
             notification = root._get_node(input.path)
@@ -35,18 +47,23 @@ class SkylightNotificationAction(Action):
 # ---------------------------------------------
 class Main(ncs.application.Application):
     '''Nano service appliction implementing the nano create callback'''
+
     def setup(self):
         # The application class sets up logging for us. It is accessible
         # through 'self.log' and is a ncs.log.Log instance.
         self.log.info('Main RUNNING')
-
+        self.register_nano_service(servicepoint='edge-servicepoint',
+                                   componenttype="streaming:edge",
+                                   state="streaming:connected-to-skylight",
+                                   nano_service_cls=ConnectedToSkylight)
         # Nano service callbacks require a registration for a service point,
         # component, and state, as specified in the corresponding data model
         # and plan outline.
 
         # If we registered any callback(s) above, the Application class
         # took care of creating a daemon (related to the service/action point).
-        self.register_action('skylight-notification', SkylightNotificationAction)
+        self.register_action('skylight-notification',
+                             SkylightNotificationAction)
 
         # When this setup method is finished, all registrations are
         # considered done and the application is 'started'.
