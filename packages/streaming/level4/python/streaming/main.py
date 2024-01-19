c4506878e594992aeb133eab4e05933c2e26a572
Ulrik Stridsman <uforsgre@cisco.com>
2024-01-19 09:56:12 +0100

    Setting operational data on the data center from the notification.

--- packages/streaming/level4/python/streaming/main.py
+++ packages/streaming/level4/python/streaming/main.py
@@ -8,7 +8,7 @@ See the README file for more information
 """
 import ncs
 from ncs.application import NanoService
-from ncs.maapi import Maapi
+from ncs.maapi import Maapi, single_write_trans
 from ncs.dp import Action
 
 import traceback
@@ -47,7 +47,12 @@ class SkylightNotificationAction(ncs.dp.Action):
             root = ncs.maagic.get_root(trans)
             notification = root._get_node(input.path)
 
-            self.log.info(f'Got notification type {notification}')
+            self.log.info(f'Got notification type {notification} {notification.jitter}')
+            with single_write_trans('admin', 'system', db=ncs.OPERATIONAL) as t:
+                r = ncs.maagic.get_root(t)
+                #root.streaming_dc[notification.dc].oper_status.jitter = notification.jitter
+                r.streaming__dc['dc0'].oper_status.jitter = notification.jitter
+                t.apply()
             return True
 
         except Exception as e:
