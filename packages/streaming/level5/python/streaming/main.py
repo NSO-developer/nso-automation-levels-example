b449de0937f1d06cda793f6e31cec5aeb14634fa
Jan Lindblad <jlindbla@cisco.com>
2024-01-20 16:46:59 +0100

    Now level5 dc selection weights jitter, energy-price against each other and takes edge-capacity into account.

--- packages/streaming/level5/python/streaming/main.py
+++ packages/streaming/level5/python/streaming/main.py
@@ -10,33 +10,48 @@ import ncs
 from ncs.application import NanoService
 from ncs.maapi import Maapi, single_write_trans
 from ncs.dp import Action
-import uuid, time, traceback, threading
+import uuid, time, traceback, threading, math
 
 class ConnectedToSkylight(NanoService):
+    JITTER_WEIGHT = 1
+    PRICE_WEIGHT = 2
+
     @NanoService.create
     def cb_nano_create(self, tctx, root, service, plan, component, state, proplist, compproplist):
-        self.log.info(f'cb_nano_create: ConnectedToSkylight')
+        self.log.info(f'cb_nano_create: ConnectedToSkylight for {service.name}')
         vars = ncs.template.Variables()
         # Create a unique session ID from the service name
         vars.add('SESSION_ID', str(uuid.uuid5(uuid.NAMESPACE_DNS,
                  f'{service.name}-edge-connected-to-skylight')))
-        # Find the DC with the lowest jitter
-        best_jitter = 100000
+
+        # Find the DC with the lowest score
+        best_score = 100000
         best_dc = None
         for n in root.dc:
-            if n.oper_status.jitter is None:
-                continue
+            if n.oper_status.jitter is None or n.oper_status.energy_price is None:
+                self.log.info(f'Checking DC {n.name}: DC is not ready')
+                continue # Data not available yet, disregard this option
+            if len(n.oper_status.edge_clients) >= n.edge_capacity and service.name not in n.oper_status.edge_clients:
+                self.log.info(f'Checking DC {n.name}: DC is full')
+                continue # Already full, not an option
             dc_jitter = float(n.oper_status.jitter)
-            self.log.info(
-                f'Checking DC {n.name} jitter {dc_jitter}')
-            if dc_jitter < best_jitter:
-                best_jitter = dc_jitter
-                best_dc = n.name
+            dc_price = int(n.oper_status.energy_price)
+            dc_score = ConnectedToSkylight.JITTER_WEIGHT * math.log10(dc_jitter) + \
+                       ConnectedToSkylight.PRICE_WEIGHT * math.log10(dc_price)
+            self.log.info(f'Checking DC {n.name}: jitter {dc_jitter} price {dc_price} -> score {dc_score}')
+            if dc_score < best_score:
+                best_score = dc_score
+                best_dc = n
         if best_dc is None:
             raise Exception('No DC found')
-        self.log.info(f'Found DC {best_dc} with the lowest jitter {best_jitter}')
         vars.add('DC', best_dc)
-        service.oper_status.chosen_dc = best_dc
+        if service.oper_status.chosen_dc:
+            self.log.info(f'Leaving DC {root.dc[service.oper_status.chosen_dc].name}')
+            root.dc[service.oper_status.chosen_dc].oper_status.edge_clients.remove(service.name)
+        service.oper_status.chosen_dc = best_dc.name
+        self.log.info(f'Using DC {root.dc[service.oper_status.chosen_dc].name}')
+        root.dc[service.oper_status.chosen_dc].oper_status.edge_clients.create(service.name)
+
         # Apply the template
         template = ncs.template.Template(service)
         template.apply('edge-servicepoint-edge-connected-to-skylight', vars)
